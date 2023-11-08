73465d9244a2975552697809fabf4ebb2b6566f8
Matthew Nolan <mdnolan@arizona.edu>
2023-11-08 08:03:06 -0700

    Test Commit 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("How are you doing?");
+        System.out.println("How are you doing?");
     }
 }
