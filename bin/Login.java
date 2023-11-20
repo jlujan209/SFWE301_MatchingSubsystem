8ce04c7a3a44483fcdf801608258e239962cb83a
Hsinwei Lin <104602727+Hsinweilin@users.noreply.github.com>
2023-11-20 14:30:10 -0700

    add view all scholarships based on criteria

--- bin/Login.java
+++ bin/Login.java
@@ -2,28 +2,65 @@ import java.util.Scanner;
 
 public class Login {
     public static void main(String[] args) {
+
+        Scholarship[] s = new Scholarship[6];
+        s[0] = new Scholarship("Outstading SFWE", 3.2f, "SFWE", 12, 13, 2023, 1000);
+        s[1] = new Scholarship("SFWE Academic distinction", 3.5f, "SFWE", 12, 23, 2023, 500);
+        s[2] = new Scholarship("SFWE Academic Excellence", 4.0f, "SFWE", 12, 23, 2023, 500);
+        s[3] = new Scholarship("Outstading ECE", 3.2f, "ECE", 12, 17, 2023, 3000);
+        s[4] = new Scholarship("Outstading SIE", 3.0f, "SIE", 12, 30, 2023, 1000);
+        s[5] = new Scholarship("SIE Academic distinction", 3.0f, "SIE", 12, 25, 2023, 5000);
+        // s[0].printScholarship();
+
         try (Scanner scnr = new Scanner(System.in)) {
 
-            System.out.println("Enter userName => ");
-            String userName = scnr.nextLine();
-            System.out.println("Enter password => ");
-            String password = scnr.nextLine();
+            boolean loggined = false;
 
-            if ("Hsinwei".equals(userName) && "abc123".equals(password)) {
-                System.out.printf("User is Successfully loggined...\n\n");
-                System.out.println("Enter search criteria minimum accepted GPA: ");
-                float gpa = scnr.nextFloat();
-                System.out.println("Enter search criteria scholarship department(s): ");
-                String department = scnr.next();
-                System.out.println("Enter search criteria selection date: month/date/year: ");
-                String date = scnr.next();
-                System.out.println("Enter search criteria rewarded amount: ");
-                int amount = scnr.nextInt();
-                System.out.printf("Search criteria: gpa: %.1f, departments: %s, date: %s, amount: %d$\n", gpa,
-                        department,
-                        date, amount);
-            } else {
-                System.out.println("Invalid userName or password...");
+            while (!loggined) {
+                System.out.println("Enter userName => ");
+                String userName = scnr.nextLine();
+                System.out.println("Enter password => ");
+                String password = scnr.nextLine();
+                if ("Hsinwei".equals(userName) && "abc123".equals(password)) {
+                    loggined = true;
+                    System.out.printf("User is Successfully loggined...\n\n");
+                    System.out.println(
+                            "Choose user action: 1 = view all available scholarships according to desiring criteria, 2 = view application status ");
+                    int action = scnr.nextInt();
+                    if (action == 1) {
+                        System.out.println("Enter search criteria minimum accepted GPA: ");
+                        float gpa = scnr.nextFloat();
+                        System.out.println("Enter search criteria scholarship department(s): ");
+                        String department = scnr.next();
+                        System.out.println("Enter search criteria selection date: month/date/year: ");
+                        String date = scnr.next();
+                        System.out.println("Enter search criteria rewarded amount: ");
+                        int amount = scnr.nextInt();
+                        System.out.printf(
+                                "Scholarships according to desiring criteria: gpa: %.1f, departments: %s, date: %s, amount: %d$\n",
+                                gpa,
+                                department,
+                                date, amount);
+                        int month = Integer.parseInt(date.substring(0, 2));
+                        int day = Integer.parseInt(date.substring(3, 5));
+                        int year = Integer.parseInt(date.substring(6, 10));
+                        // System.out.printf("%d/%d/%d\n", month, day, year);
+                        for (int i = 0; i < s.length; i++) {
+                            if (s[i].GPA >= gpa && s[i].department.equals(department)
+                                    && (s[i].month > month || s[i].year > year
+                                            || (s[i].month == month && s[i].year == year && s[i].date >= day))
+                                    && s[i].amount >= amount) {
+                                s[i].printScholarship();
+                            }
+                        }
+                    } else if (action == 2) {
+
+                    } else {
+
+                    }
+                } else {
+                    System.out.println("Please login again");
+                }
             }
         } catch (Exception err) {
             System.out.println(err);
---
+++ bin/Scholarship.java
@@ -0,0 +1,23 @@
+public class Scholarship {
+    public String scholarshipName;
+    public float GPA;
+    public String department;
+    public int month;
+    public int date;
+    public int year;
+    public int amount;
+
+    Scholarship(String scholarshipName, float GPA, String department, int month, int date, int year, int amount){
+        this.scholarshipName = scholarshipName;
+        this.GPA = GPA;
+        this.department = department;
+        this.month = month;
+        this.date = date;
+        this.year = year;
+        this.amount = amount;
+    }
+
+    public void printScholarship() {
+        System.out.printf("Name: %s, GPA: %.1f, Department: %s, Due date: %d/%d/%d, Amount: %d$ \n", scholarshipName, GPA, department, month, date, year, amount);
+    }
+}
